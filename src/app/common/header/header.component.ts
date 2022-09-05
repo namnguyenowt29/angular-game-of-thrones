990c0423909d235da041fa1eefdb55eb18bf680a
namnguyenowt29 <nnguyenquoc@openwt.com>
2022-09-05 14:36:32 +0700

    add search element to the page

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,5 +1,8 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +11,45 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   visible = false;
-  constructor(private router: Router) {}
+  isSearchModalVisible = false;
+  searchForm!: FormGroup;
+  resources = ['books', 'houses', 'characters'];
+
+  constructor(private router: Router, private apiService: ApiService) {}
   ngOnInit(): void {
     this.router.events.subscribe(() => {
       this.visible = false;
+    });
+
+    this.searchForm = new FormGroup({
+      resourceName: new FormControl(null),
+      name: new FormControl(null),
     });
   }
 
   onClickMenu(): void {
     this.visible = !this.visible;
+  }
+
+  handleSearchBox() {
+    this.isSearchModalVisible = true;
+  }
+  handleCancel() {
+    this.isSearchModalVisible = false;
+  }
+  handleSubmitSearch() {
+    let searchParams = new HttpParams();
+    this.isSearchModalVisible = false;
+    let formValues = Object(this.searchForm.value);
+
+    searchParams = searchParams.append('name', formValues['name']);
+
+    this.apiService
+      .fetchResource('/' + formValues['resourceName'], searchParams)
+      .subscribe({
+        next: data => console.log(data),
+        error: err => console.log(err),
+      });
+    this.searchForm.reset();
   }
 }
--- src/app/template/main-template/main-template.component.ts
+++ src/app/template/main-template/main-template.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-template',
   templateUrl: './main-template.component.html',
-  styleUrls: ['./main-template.component.scss']
+  styleUrls: ['./main-template.component.scss'],
 })
 export class MainTemplateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
